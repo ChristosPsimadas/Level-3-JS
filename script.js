cfca53840150da545fc043171669738890b3ed8c
Christos Psimadas <75281423+ChristosPsimadas@users.noreply.github.com>
2022-12-14 13:36:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-function initialize(){
-    diceTable = document.getElementById("dicetable");
-    rollDice();
-}
+function getRandomInteger(lower, upper){
+    var multiplier = upper - (lower - 1);
+    var rnd = parseInt(Math.random() * multiplier) + lower;
 
-function rollDice(){
-    var numRolls = getRandomIneger(25, 100);
-
-    for(var i = 0; i < numRolls; i++){
-        var dieRoll = getRandomIneger(1,6);
-
-        var newRow = diceTable.insertRow();
-        var newCell = newRow.insertCell();
-        newCell.innerHTML = "Roll " + (i + 1);
-
-        newCell = newRow.insertCell();
-        newCell.innerHTML = diceRoll;
-    }
+    return rnd;
 }
